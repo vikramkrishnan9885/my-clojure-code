0fbc5b90c26fc9f4594c5bb5c74519431a301675
Vikram Krishnan <kvikram9885@gmail.com>
2017-09-29 20:13:38 +0530

    We edited core and added main function to it, we also edited project.clj to use core as location for main

--- learnclojure/project.clj
+++ learnclojure/project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main learnclojure.core ;; This specifies the location of the main function. Without this, we have to run 'lein run -m learnclojure.core'
+)
--- learnclojure/src/learnclojure/core.clj
+++ learnclojure/src/learnclojure/core.clj
@@ -1,6 +1,11 @@
-(ns learnclojure.core)
+(ns learnclojure.core) ;; Look at this name space
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+;; To create an executable we need a -main function similar to Java main
+(defn -main []
+  (foo "Main")
+)
