5c4ec4be3a871c0ef9a417210499d4ff3e2a0b2a
Vikram Krishnan <kvikram9885@gmail.com>
2017-10-01 16:51:59 +0530

    Basic Routing with Compojure

--- clojureweb/learn-compojure/src/learn_compojure/core.clj
+++ clojureweb/learn-compojure/src/learn_compojure/core.clj
@@ -1,7 +1,23 @@
 (ns learn-compojure.core
-  (:gen-class))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defroutes myapp ;; defroutes is a compojure macro that creates a ring handler
+  (GET "/" [] "Hello from Compojure")
+  (POST "/" [] "Posted to / boilerplate") ;; you can define all rest methods thusly
+  (POST "/:name" [name] (str "Hello, " name)) ;; POST "/route/:variable" and then pass that variable as an argument to [argument] and use it in your function
+  (POST "/abc/:name"  req (str req))
+  (GET "/route1" [] "Hello from Vikram")
+  (route/not-found "<h1>Page not found</h1>") ;; default 404
+)
+
+;; You can use this for testing and debugging
+(myapp {:uri "/" :request-method :get})
+(myapp {:uri "/" :request-method :post})
+(myapp {:uri "/steve" :request-method :post})
+(myapp {:uri "/abc/steve" :request-method :post})
+
+(defn -main []
+  (jetty/run-jetty myapp {:port 3000}))
