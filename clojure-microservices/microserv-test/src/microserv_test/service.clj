d8bfc07c4a61ed48d295bf12f585c02e8bc7433d
Vikram Krishnan <kvikram9885@gmail.com>
2017-10-07 20:54:16 +0400

    Parameterized GET

--- clojure-microservices/microserv-test/src/microserv_test/service.clj
+++ clojure-microservices/microserv-test/src/microserv_test/service.clj
@@ -45,6 +45,38 @@
   (http/json-response mock-project-collection)) ;; THIS ENSURES THAT WE GET A JSON RESPONSE
 
 
+;; PARAMETERIZED GET
+
+(
+  defn get-project [request]
+    (
+      let [projname (get-in request [:path-params :project-name])] ;; HOWEVER, GETTING NESTED VALUES IS COMMON ENOUGH THAT CLOJURE PROVIDES A FUNCTION DESIGNED SPECIFICALLY TO ADDRESS THAT NEED: GET-IN. THE GET-IN FUNCTION RETURNS THE VALUE IN A NESTED ASSOCIATIVE STRUCTURE WHEN GIVEN A SEQUENCE OF KEYS. USING GET-IN YOU CAN REPLACE THE LAST EXAMPLE WITH THE FOLLOWING CODE. (-> json-data read-json (get-in ["scores" "FSU"])) .. user=> (get-in {"timestamp" 1291578985220 "scores" {"FSU" 31 "UF" 7}} ["scores" "FSU"]) GIVES US 31
+        ;; EXAMPLES OF GET-IN
+        ;; We can use get-in for reaching into nested maps:
+        ;; user=> (def m {:username "sally"
+        ;;       :profile {:name "Sally Clojurian"
+        ;;                 :address {:city "Austin" :state "TX"}}})
+        ;; user=> (get-in m [:profile :name])
+        ;; "Sally Clojurian"
+        ;; user=> (get-in m [:profile :address :city])
+        ;; "Austin"
+        ;;
+        ;; NOW WHAT ARE WE SELECTING FOR USING GET-IN
+        ;; REMEMBER THAT WHEN WE PRINTED OUR REQUEST WE GOT
+        ;; INFO  io.pedestal.http.cors - {:msg "cors request processing", :origin "", :allowed true, :line 84}
+        ;; {:protocol "HTTP/1.1", :async-supported? true, :remote-addr "127.0.0.1", :servlet-response #object[org.eclipse.jetty.server.Response 0xf2e0986 "HTTP/1.1 200 \nDate: Sat, 07 Oct 2017 14:53:15 GMT\r\n\r\n"], :servlet #object[io.pedestal.http.servlet.FnServlet 0x509e22f4 "io.pedestal.http.servlet.FnServlet@509e22f4"], :headers {"user-agent" "curl/7.52.1", "accept" "*/*", "host" "localhost:5000", "origin" ""}, :server-port 5000, :servlet-request #object[org.eclipse.jetty.server.Request 0x3158fdeb "Request(GET //localhost:5000/)@3158fdeb"], :path-info "/", :url-for #object[clojure.lang.Delay 0xfee9e05 {:status :pending, :val nil}], :uri "/", :server-name "localhost", :query-string nil, :path-params [], :body #object[org.eclipse.jetty.server.HttpInputOverHTTP 0x6448b75f "HttpInputOverHTTP@6448b75f[c=0,q=0,[0]=null,s=STREAM]"], :scheme :http, :request-method :get}
+        ;; WE ARE TRYING TO EXTRACT PROJECT-NAME FROM THE PATH-PARAMS
+        ;;
+        (
+          http/json-response ((keyword projname) mock-project-collection)
+          ;; KEYWORD Returns a Keyword with the given namespace and name.
+          ;; FOR EXAMPLE
+          ;; user=> (keyword 'foo)
+          ;; :foo
+        )
+    )
+)
+
 ;; Defines "/" and "/about" routes with their associated :get handlers.
 ;; The interceptors defined after the verb map (e.g., {:get home-page}
 ;; apply to / and its children (/about).
@@ -53,6 +85,7 @@
 ;; Tabular routes
 (def routes #{["/" :get (conj common-interceptors `home-page)]
               ["/projects" :get (conj common-interceptors `get-projects)] ;; ADD OUR GET ENDPOINT, NEED FURTHER STUDY TO SEE HOW THE CODE BELOW DIFFERS FROM THE TABLE THINGIE WE HAVE HERE
+              ["/projects/:project-name" :get (conj common-interceptors `get-project)] ;; END POINT FOR PARAMETERIZED GET
               ["/about" :get (conj common-interceptors `about-page)]})
 
 ;; Map-based routes
